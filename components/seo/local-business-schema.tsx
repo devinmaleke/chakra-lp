ce70113025905b5568ad6460a0f90d93dc6d9347
Devin <devin.maleke@gmail.com>
2026-07-04 13:44:07 +0700

    fixed: Opengraph optimized

--- components/seo/local-business-schema.tsx
+++ components/seo/local-business-schema.tsx
@@ -3,51 +3,51 @@
 export default function LocalBusinessSchema() {
   const schema = {
     "@context": "https://schema.org",
-    "@type": "HealthAndBeautyBusiness",
-
-    name: "Chakra Reflexology",
-
-    image: "https://chakrareflexology.com/og-image.jpg",
-
-    url: "https://chakrareflexology.com",
-
-    telephone: "+6282177980464",
-
-    address: {
-      "@type": "PostalAddress",
-      streetAddress: "Jl. Citayam No. 87A-B",
-      addressLocality: "Depok",
-      addressRegion: "Jawa Barat",
-      postalCode: "16431",
-      addressCountry: "ID",
-    },
-
-    geo: {
-      "@type": "GeoCoordinates",
-      latitude: "-6.4400",
-      longitude: "106.8150",
-    },
-
-    openingHoursSpecification: [
+    "@graph": [
       {
-        "@type": "OpeningHoursSpecification",
-        dayOfWeek: [
-          "Monday",
-          "Tuesday",
-          "Wednesday",
-          "Thursday",
-          "Friday",
-          "Saturday",
-          "Sunday",
+        "@type": "HealthAndBeautyBusiness",
+        name: "Chakra Reflexology",
+        image: "https://chakrareflexology.com/og-image.jpg",
+        url: "https://chakrareflexology.com",
+        telephone: "+6282177980464",
+        address: {
+          "@type": "PostalAddress",
+          streetAddress: "Jl. Citayam No. 87A-B",
+          addressLocality: "Depok",
+          addressRegion: "Jawa Barat",
+          postalCode: "16431",
+          addressCountry: "ID",
+        },
+        geo: {
+          "@type": "GeoCoordinates",
+          latitude: "-6.4400",
+          longitude: "106.8150",
+        },
+        openingHoursSpecification: [
+          {
+            "@type": "OpeningHoursSpecification",
+            dayOfWeek: [
+              "Monday",
+              "Tuesday",
+              "Wednesday",
+              "Thursday",
+              "Friday",
+              "Saturday",
+              "Sunday",
+            ],
+            opens: "08:30",
+            closes: "23:59",
+          },
         ],
-        opens: "08:30",
-        closes: "23:59",
+        priceRange: "$$",
+        sameAs: ["https://instagram.com/chakrafamilymassage"],
+      },
+      {
+        "@type": "WebSite",
+        name: "Chakra Reflexology",
+        url: "https://chakrareflexology.com",
       },
     ],
-
-    priceRange: "$$",
-
-    sameAs: ["https://instagram.com/chakrafamilymassage"],
   };
 
   return (
